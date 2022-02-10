68d9bbaf549a45208070996dd9a960e04f4a8683
Gohome <tueminh6492@gmail.com>
2022-02-10 21:02:37 +0700

    Fix Netlify routing

--- client/src/contexts/constants.js
+++ client/src/contexts/constants.js
@@ -1,7 +1,8 @@
 export const apiUrl =
   process.env.NODE_ENV !== "production"
     ? "http://localhost:5000/api"
-    : "somedeployUrl";
+    : 
+      "change duong dan heroku.com";
 
 export const LOCAL_STORAGE_TOKEN_NAME = "learnnit-mern";
 
@@ -12,3 +13,6 @@ export const ADD_POST = 'ADD_POST'
 export const DELETE_POST =  'DELETE_POST'
 export const UPDATE_POST =  'UPDATE_POST'
 export const FIND_POST = "FIND_POST";
+
+
+//change duong dan
